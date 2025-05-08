2c5b3deb9d2859afa890e387d646fcf093fe0804
Skyler <mclainskyler@gmail.com>
2025-05-08 09:03:58 -0400

    fixing routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { ContextProvider } from './context/ContextProvider';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ContextProvider>
         <App />
       </ContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
